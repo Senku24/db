c9078872efb3faae7967b37da08360459486f997
Senku24 <nixonpaule24@gmail.com>
2026-07-19 03:19:22 +0530

    sql in nodejs done

--- postgres/index.js
+++ postgres/index.js
@@ -2,12 +2,35 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const authMiddleware = require('./authMiddleware');
-
+const { Pool } = require('pg');
+const pool  = new Pool({
+    connectionString: 'postgresql://neondb_owner:npg_1V4FTQKEZULR@ep-flat-firefly-ataekle7-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+})
+// pool.connect()
 
 app.use(express.json());
 
-app.post('/signup', (req, res) => {});
-app.post('/signin', (req, res) => {});
+app.post('/signup', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    const response = await pool.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id', [username, email, password]);
+
+    res.json ({
+        message: 'User registered successfully',
+        userId: response.rows[0].id
+    })
+});
+app.post('/signin', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    const response = await pool.query('SELECT * FROM users WHERE username = $1 AND email = $2 AND password = $3', [username, email, password]);
+    const userExists = response.rows[0];
+    if(!userExists) {
+        return res.status(403).json({ message: 'Invalid credentials' });
+    }
+    const token = jwt.sign({userId: userExists.id}, 'secret_key', { expiresIn: '1h' });
+    res.json({ token });
+});
 
 
 
