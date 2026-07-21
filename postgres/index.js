0f201aa081c49a574ff76d5f0a9399c56ded6926
Senku24 <nixonpaule24@gmail.com>
2026-07-22 03:21:11 +0530

    zod&bcrypt- hashing&Validation

--- postgres/index.js
+++ postgres/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const z = require('zod');
 const port = 3000;
 const authMiddleware = require('./authMiddleware');
 const { Pool } = require('pg');
@@ -10,10 +13,23 @@ const pool  = new Pool({
 
 app.use(express.json());
 
-app.post('/signup', async (req, res) => {
-    const { username, email, password } = req.body;
+const signupSchema = z.object({
+    username: z.string().min(3).max(20),
+    email: z.string().email(),
+    password: z.string().min(6).max(20)
+});
 
-    const response = await pool.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id', [username, email, password]);
+app.post('/signup', async (req, res) => {
+    const {data, success, error} = signupSchema.safeParse(req.body);
+    if(!success) {
+        return res.status(402 ).json({ message: 'Invalid input', error: error.errors });
+    }
+    const username = data.username;
+    const email = data.email;
+    const password = data.password;
+    const hashedpassword = await bcrypt.hash(password, 10);
+
+    const response = await pool.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id', [username, email, hashedpassword]);
 
     res.json ({
         message: 'User registered successfully',
@@ -21,11 +37,16 @@ app.post('/signup', async (req, res) => {
     })
 });
 app.post('/signin', async (req, res) => {
-    const { username, email, password } = req.body;
+    const { username, email} = req.body;
+    const password = req.body.password;
 
-    const response = await pool.query('SELECT * FROM users WHERE username = $1 AND email = $2 AND password = $3', [username, email, password]);
+    const response = await pool.query('SELECT * FROM users WHERE username = $1 AND email = $2', [username, email]);
     const userExists = response.rows[0];
     if(!userExists) {
+        return res.status(403).json({ message: 'Invalid credentials' });
+    }
+    const isPasswordValid = await bcrypt.compare(password, userExists.password);
+    if(!isPasswordValid) {
         return res.status(403).json({ message: 'Invalid credentials' });
     }
     const token = jwt.sign({userId: userExists.id}, 'secret_key', { expiresIn: '1h' });
