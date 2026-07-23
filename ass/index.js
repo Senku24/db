793b4e46aa0e1239981e7bacfdfff4fc186f8498
Senku24 <nixonpaule24@gmail.com>
2026-07-23 22:15:28 +0530

    endpoints created

--- ass/index.js
+++ ass/index.js
@@ -66,12 +66,46 @@ app.post('/login', async (req, res) => {
     const token = jwt.sign({userId: userExists.id}, process.env.JWT_SECRET, { expiresIn: '1h' });
     res.json({ message: 'Login successful', token });
 });
-app.post('/transfer', authMiddleware, async (req, res) => {});
+app.post('/transfer', authMiddleware, async (req, res) => {
+    const userId = req.userId;
+    const accId = req.body.accId;
+    const amount = req.body.amount;
+
+    const accountExists = await pool.query('SELECT * FROM account WHERE id = $1', [accId]);
+    if(accountExists.rows.length === 0) {
+        return res.status(404).json({ message: 'Account not found' });
+    }
+    const senderAccount = await pool.query('SELECT * FROM account WHERE userId = $1', [userId]);
+    if(senderAccount.rows.length === 0) {
+        return res.status(404).json({ message: 'Sender account not found' });
+    }
+    if(senderAccount.rows[0].balance < amount) {
+        return res.status(400).json({ message: 'Insufficient balance' });
+    }
+    const updateSenderBalance = await pool.query('UPDATE account SET balance = balance - $1 WHERE userId = $2', [amount, userId]);
+    const updateReceiverBalance = await pool.query('UPDATE account SET balance = balance + $1 WHERE id = $2', [amount, accId]);
+
+    res.json({ message: 'Transfer successful' });
+});
 
 // read endpoints
-app.get('/view-balance', authMiddleware, async (req, res) => {});
-app.get('/view-user', authMiddleware, async (req, res) => {});
-app.get('/view-all-users', authMiddleware, async (req, res) => {});
+app.get('/view-balance', authMiddleware, async (req, res) => {
+    const userId = req.userId;
+    const balance = await pool.query('SELECT balance FROM account WHERE userId = $1', [userId]);
+    if(balance.rows.length === 0) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+    res.json({ balance: balance.rows[0].balance });
+});
+app.get('/view-user', authMiddleware, async (req, res) => {
+    const userId = req.userId;
+    const username = await pool.query('SELECT username FROM users WHERE id = $1', [userId]);
+    res.json({ username: username.rows[0].username });
+});
+app.get('/view-all-users', authMiddleware, async (req, res) => {
+    const allUsers = await pool.query('SELECT username FROM users');
+    res.json({ users: allUsers.rows });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
